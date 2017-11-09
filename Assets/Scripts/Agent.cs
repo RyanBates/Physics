bfddb9c6bdb03f4e0b6de6fee5f1c1bdbace8157
Ryan Bates <rbates214@gmail.com>
2017-11-08 18:00:16 -0600

    created rules

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Ryan
+{
+    [CreateAssetMenu]
+    public abstract class Agent : ScriptableObject
+    {
+        public float mass, acceleration, max_speed;
+
+        public Vector3 seperation, alignment, cohesion;
+
+        public Vector3 velocity, position;
+
+        public abstract Vector3 Update_Agent();
+
+        public abstract bool AddForce(float a, Vector3 b);
+    }
+}
---
+++ Assets/Scripts/AgentBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AgentBehaviour : MonoBehaviour
+{
+    Ryan.Agent agent;
+
+    public List<Ryan.Agent> agents;
+    public List<AgentBehaviour> agentBehaviour;
+
+
+    [ContextMenu("Create")]
+    public void Create()
+    {
+        agents = new List<Ryan.Agent>();
+        agentBehaviour = new List<AgentBehaviour>();
+
+        
+    }
+
+
+    [ContextMenu("Destory")]
+    public void Destory()
+    {
+        agents.Clear();
+        agentBehaviour.Clear();
+    }
+}
