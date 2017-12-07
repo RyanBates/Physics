f57ab490b39699d014ed187b5d03ed9c6bc03bbb
Ryan Bates <rbates214@gmail.com>
2017-12-06 18:01:15 -0600

    made the cloth structure
    made cloth spawn the particles

---
+++ Assets/Scripts/Ryan/Cloth/ParticleBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+public class ParticleBehaviour : MonoBehaviour
+{
+    public HooksLaw.Particle particle;
+
+    
+    void Start()
+    {
+        particle = new HooksLaw.Particle(transform.position, Vector3.zero, 1);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = particle.Update(Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/Ryan/Cloth/SpringDamperBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpringDamperBehaviour : MonoBehaviour
+{
+    ClothSize cloth;
+    HooksLaw.SpringDamper Sd;
+
+    HooksLaw.Particle p1;
+    HooksLaw.Particle p2;
+
+    public List<ParticleBehaviour> particles;
+    public List<HooksLaw.SpringDamper> springDampers;
+
+    void UpdateParticles(HooksLaw.Particle part1, HooksLaw.Particle part2)
+    {
+        Sd = new HooksLaw.SpringDamper(part1, part2, 5, .7f, 10);
+        springDampers.Add(Sd);
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < particles.Count - 1; i++)
+        {
+            p1 = particles[i].particle;
+            p2 = particles[i + 1].particle;
+
+            if (!(i != 0) && (i + 1) % cloth.size == 0)
+                UpdateParticles(p1, p2);
+        }
+
+        for (int i = 0; i <= particles.Count - 1; i++)
+        {
+            p1 = particles[i].particle;
+            p2 = particles[i + (int)cloth.size - 1].particle;
+
+            UpdateParticles(p1, p2);
+        }
+    }
+
+    void Update()
+    {
+        foreach (HooksLaw.SpringDamper s in springDampers)
+        {
+            Sd.CalculateForce();
+        }
+    }
+}
